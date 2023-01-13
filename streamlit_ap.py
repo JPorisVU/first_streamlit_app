db214e0ff7146db3fdc09e295eb4edf149989275
JP <122056137+jpsftrain@users.noreply.github.com>
2023-01-13 10:31:56 -0600

    Update streamlit_ap.py
    cleaning up and normalizing fruityvice display detail

--- streamlit_ap.py
+++ streamlit_ap.py
@@ -29,8 +29,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #shows base json data on screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi)
 
 # Normalize json data 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
